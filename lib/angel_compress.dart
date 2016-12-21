4e3947a290808e68f2f0dae6d24989e91439ec5b
Tobe O <thosakwe@gmail.com>
2016-12-21 08:16:03 -0500

    Update angel_compress.dart

--- lib/angel_compress.dart
+++ lib/angel_compress.dart
@@ -4,34 +4,41 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:angel_framework/angel_framework.dart';
 
+/// Calls [compress] with `'gzip'` and the [GZIP] codec.
+RequestHandler gzip({bool recompress: false, Map<String, String> headers: const {}}) =>
+    compress('gzip', GZIP, recompress: recompress, headers: headers);
+
 /// When `Accept-Encoding` contains '*' or [contentEncoding], compresses the
 /// response buffer with the given [codec]. Any [headers] will be sent as well.
 ///
 /// The `Content-Encoding` header will be set to [contentEncoding] as well.
+///
+/// If `Content-Encoding` was already set and [recompress] is not true, no
+/// additional compression will be added.
 RequestHandler compress(
     String contentEncoding, Codec<List<int>, List<int>> codec,
-    {Map<String, String> headers: const {}}) {
+    {bool recompress: false, Map<String, String> headers: const {}}) {
   return (RequestContext req, ResponseContext res) async {
-    print('Incoming: ${req.headers}');
-    
-    List<String> allowedEncodings =
-        req.headers.value(HttpHeaders.ACCEPT_ENCODING) != null
-            ? req.headers
-                .value(HttpHeaders.ACCEPT_ENCODING)
-                .split(',')
-                .map((str) => str.trim().toLowerCase())
-                .toList()
-            : ['*'];
+    if (!res.headers.containsKey(HttpHeaders.CONTENT_ENCODING) || recompress) {
+        List<String> allowedEncodings =
+            req.headers.value(HttpHeaders.ACCEPT_ENCODING) != null
+                ? req.headers
+                    .value(HttpHeaders.ACCEPT_ENCODING)
+                    .split(',')
+                    .map((str) => str.trim().toLowerCase())
+                    .toList()
+                : ['*'];
 
-    for (String encoding in allowedEncodings) {
-      if (encoding == '*' || encoding == contentEncoding) {
-        res.headers.addAll(headers ?? {});
-        res.headers[HttpHeaders.CONTENT_ENCODING] = contentEncoding;
-        var bytes = res.buffer.takeBytes();
-        var encoded = codec.encode(bytes);
-        res.buffer.add(encoded);
-        break;
-      }
+        for (String encoding in allowedEncodings) {
+          if (encoding == '*' || encoding == contentEncoding) {
+            res.headers.addAll(headers ?? {});
+            res.headers[HttpHeaders.CONTENT_ENCODING] = contentEncoding;
+            var bytes = res.buffer.takeBytes();
+            var encoded = codec.encode(bytes);
+            res.buffer.add(encoded);
+            break;
+          }
+        }
     }
 
     return true;
